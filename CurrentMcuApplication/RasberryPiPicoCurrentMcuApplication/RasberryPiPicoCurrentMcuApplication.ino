b9cf6f2f7c1414e7e02488d797853e6078424ed7
mametarou963 <mametarou967@gmail.com>
2025-08-31 16:57:53 +0900

    add mux string

--- CurrentMcuApplication/RasberryPiPicoCurrentMcuApplication/RasberryPiPicoCurrentMcuApplication.ino
+++ CurrentMcuApplication/RasberryPiPicoCurrentMcuApplication/RasberryPiPicoCurrentMcuApplication.ino
@@ -3,6 +3,7 @@
 #define ADS1105_ADDR 0x48  // ADDR=GND の場合
 
 void setup() {
+  uint16_t config;
   Serial.begin(115200);
   Wire.begin(); // SDA=GP4, SCL=GP5 (Picoでは自動割り当てされます)
 
@@ -19,11 +20,43 @@ void setup() {
 
   Wire.requestFrom(ADS1105_ADDR, 2);
   if (Wire.available() == 2) {
-    uint16_t config = (Wire.read() << 8) | Wire.read();
+    config = (Wire.read() << 8) | Wire.read();
     Serial.print("ADS1105 Config Reg = 0x");
     Serial.println(config, HEX);
   } else {
     Serial.println("Read error");
+  }
+
+  {
+    uint8_t muxValue = (config >> 12) & 0x7;
+    Serial.print("MUX = ");
+    Serial.println(getMuxString(muxValue));
+  }
+}
+
+const char* getMuxString(uint8_t mux)
+{
+  switch(mux)
+  {
+    case 0b00000000:
+      return "AINP = AIN0 and AINN = AIN1 (default)";
+    case 0b00000001:
+      return "AINP = AIN0 and AINN = AIN3";
+    case 0b00000010:
+      return "AINP = AIN1 and AINN = AIN3";
+    case 0b00000011:
+      return "AINP = AIN2 and AINN = AIN3";
+    case 0b00000100:
+      return "AIN0 and AINN = GND";
+    case 0b00000101:
+      return "AINP = AIN1 and AINN = GND";
+    case 0b00000110:
+      return " AINP = AIN2 and AINN = GND";
+    case 0b00000111:
+      return " AINP = AIN3 and AINN = GND";
+    default:
+      return "";
+
   }
 }
 
